b36b65c6d03e1a4c8d116503e3a6dfc8efac27d3
Pedro Puertas Rodríguez <pedropuertasr@gmail.com>
2022-04-21 14:24:12 +0200

    b

--- Lenguaje/Tema 8/nodo.js
+++ Lenguaje/Tema 8/nodo.js
@@ -1,2 +1,8 @@
-let primerParrafo = document.getElementById("primero");
-alert(primerParrafo.innerHTML);
+/*let primerParrafo = document.getElementById("primero");
+alert(primerParrafo.innerHTML);*/
+
+let parrafos = document.querySelectorAll('p');
+
+for(let p of parrafos){
+    p.innerHTML = prompt("Dime el contenido");
+}
