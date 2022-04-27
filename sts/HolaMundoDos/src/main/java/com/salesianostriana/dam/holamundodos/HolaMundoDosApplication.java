b7567853685c75cbc4c349ba23959f53bae74c80
Pedro Puertas Rodríguez <pedropuertasr@gmail.com>
2022-04-27 12:34:44 +0200

    Ejercicios sts y trabajo final

---
+++ sts/HolaMundoDos/src/main/java/com/salesianostriana/dam/holamundodos/HolaMundoDosApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.holamundodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoDosApplication.class, args);
+	}
+
+}
